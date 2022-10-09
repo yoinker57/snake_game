c946b9e75c4724de1091688000874fa86d132efe
yoinker57 <93473651+yoinker57@users.noreply.github.com>
2022-10-09 18:34:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,5 @@ while game_is_on:
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
+            
             score.game_over()
